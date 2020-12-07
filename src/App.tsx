6b0e2bf8fff3adb434c22ea76375d80f6f99c378
nzambello <nzambello@protonmail.com>
2020-12-07 18:38:59 +0100

    feat(state): add appState and demo data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
 import * as React from 'react';
-import { GeistProvider, CssBaseline } from '@geist-ui/react'
+import { GeistProvider, CssBaseline } from '@geist-ui/react';
 
-const App = () => (
-  <GeistProvider>
-    <CssBaseline />
-    <p>Tocca per iniziare</p>
-  </GeistProvider>
-)
+type AppState = 'DEFAULT' | 'LOADING' | 'FINAL';
+
+const App = () => {
+  const [appState, setAppState] = React.useState<AppState>('DEFAULT');
+
+  const data = {
+    ads: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300'
+    ]
+  };
+
+  return (
+    <GeistProvider>
+      <CssBaseline />
+
+      {appState === 'DEFAULT' && (
+        <div>
+          <a
+            href="https://caferati.me/demo/react-awesome-slider/autoplay"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            slider docs
+          </a>
+          {/* data.ads */}
+          <button onClick={() => setAppState('LOADING')}>
+            Tocca per iniziare
+          </button>
+        </div>
+      )}
+      {appState === 'LOADING' && (
+        <button onClick={() => setAppState('FINAL')}>Finito</button>
+      )}
+      {appState === 'FINAL' && <p>Yey</p>}
+    </GeistProvider>
+  );
+};
 
 export default App;
