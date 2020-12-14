fff95a5e1312a00ce0f8da608cc09c4541e42dc7
nzambello <nzambello@protonmail.com>
2020-12-14 20:32:36 +0100

    feat: add slider in default view

--- src/components/DefaultView/index.tsx
+++ src/components/DefaultView/index.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
 import { IAd } from '../App';
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './index.scss';
 
 interface DefaultStateProps {
@@ -7,15 +11,36 @@ interface DefaultStateProps {
   changeState: (action: number) => void;
 }
 
+const sliderSettings = {
+  dots: false,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  adaptiveHeight: true,
+  pauseOnHover: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+};
+
 const DefaultView: React.FC<DefaultStateProps> = ({
   ads = [],
   changeState
 }) => (
   <div className="default-view">
-    {ads.length > 0 && (
-      <figure>
-        <img alt={ads[0].title} src={`http://localhost:5000${ads[0].url}`} />
-      </figure>
+    {ads?.length > 0 && (
+      <Slider {...sliderSettings}>
+        {ads?.map((ad, index) => (
+          <figure key={ad.title + index}>
+            <img
+              alt={ad.title}
+              src={`http://localhost:5000${ad.url}`}
+              title={ad.description}
+            />
+          </figure>
+        ))}
+      </Slider>
     )}
     <div className="button-wrapper">
       <button className="start-button" onClick={() => changeState(1)}>
